4ec6aa3dc8556ae7b7d318f8eb5e5453dca8d79c
Nezyro <xipear1000@gmail.com>
2024-06-14 15:02:50 +0200

    sesion 1.2

--- profesores/index_pro.php
+++ profesores/index_pro.php
@@ -1,5 +1,7 @@
 <?php
-
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
 if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     header("location: login.php");
     exit;
