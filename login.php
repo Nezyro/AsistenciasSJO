d420ff43121a426dcc3a2ea21ff79aead2db161d
Nezyro <xipear1000@gmail.com>
2024-06-14 14:51:16 +0200

    login 2

--- login.php
+++ login.php
@@ -3,13 +3,12 @@ if (session_status() == PHP_SESSION_NONE) {
     session_start();
 }
 
-
 if (isset($_POST["logout"])) {
     $_SESSION = array();
 
     if (ini_get("session.use_cookies")) {
         $params = session_get_cookie_params();
-        setcookie(session_name(), '', time() - 0,
+        setcookie(session_name(), '', time() - 42000, 
             $params["path"], $params["domain"],
             $params["secure"], $params["httponly"]
         );
